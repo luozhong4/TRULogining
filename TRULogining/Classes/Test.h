aa3eafa01c0d31a3e7d3582cc3ef0bf2b8952779
luozhong <929269921@qq.com>
2021-08-09 18:21:26 +0800

    init project

---
+++ TRULogining/Classes/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by 罗众 on 2021/8/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRULogining/Classes/Test.m
@@ -0,0 +1,12 @@
+//
+//  Test.m
+//  Test
+//
+//  Created by 罗众 on 2021/8/9.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
